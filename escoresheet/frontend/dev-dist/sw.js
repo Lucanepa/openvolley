4dde5306cd1eab958e1557a10d4469983cd9a0c5
Luca Canepa <luca.canepa@gmail.com>
2025-12-09 12:06:16 +0100

    Update settings.local.json to include additional allowed Bash commands for railway operations

--- escoresheet/frontend/dev-dist/sw.js
+++ escoresheet/frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-a959eb95'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.uuvbesuc0c"
+    "revision": "0.h2tg02k4qbo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
