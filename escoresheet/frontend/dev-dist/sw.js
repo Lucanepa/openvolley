2351604e0768779304acc615e6539bcee3a10978
Luca Canepa <luca.canepa@gmail.com>
2025-12-13 12:31:55 +0100

    Update service worker revision and remove obsolete package-lock.json
    - Updated the service worker revision in sw.js to ensure proper cache management.
    - Removed the package-lock.json file from scoresheet_pdf as it is no longer needed.
    - Added a new entry in CHANGELOG.js for version 0.7.1, documenting the changes made to enhance print functionality in the scoresheet.
    
    These changes contribute to improved cache management and streamline the project structure.

--- escoresheet/frontend/dev-dist/sw.js
+++ escoresheet/frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-a959eb95'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.mk71iam5hsg"
+    "revision": "0.phe72tb87ho"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
